56efbfa3b160c8cf455ea90364eeede3fa928c63
Adriano Sales Santos <sales.adriano@gmail.com>
2022-10-29 21:57:55 -0500

    Ajuste Script

--- publicar.sh
+++ publicar.sh
@@ -4,8 +4,15 @@ git config --global user.name "${GITLAB_USER_NAME}"
 git clone git@${CI_SERVER_HOST}:${CI_PROJECT_PATH}.git /prod
 cd /prod
 git switch prod
-sed -i "s|- name: .*$|- name: $CI_PROJECT_NAME${CI_PIPELINE_ID}|g" /prod/rancher/kubernetes_web.yml
-sed -i "s|image: .*$|image: $CI_REGISTRY/$CI_REGISTRY_USER/$CI_PROJECT_NAME|g" /prod/rancher/kubernetes_web.yml
+FILES="$@"
+for f in $FILES
+do  
+    if [ -f ${f} ]
+    then
+      sed -i "s|- name: .*$|- name: $CI_PROJECT_NAME${CI_PIPELINE_ID}|g" /prod/${f}
+      sed -i "s|image: .*$|image: $CI_REGISTRY/$CI_REGISTRY_USER/$CI_PROJECT_NAME|g" /prod/${f}
+    fi
+done
 git add .
 git commit -m "Publicação do Pipeline nº ${CI_PIPELINE_ID}"
 git push
